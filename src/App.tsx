fb462d872db4969a32fcdf166cf1e2b177d582e1
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-02-13 19:18:32 +0530

    Committing src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { FrontPage } from "./components/layout/FrontPage";
+import { LandingPage } from "./components/layout/LandingPage";
+import { ContextProvider } from "./components/ui/context/ContextProvider";
+
+function App() {
+  return (
+    <ContextProvider>
+      <div className="mx-auto flex">
+        <LandingPage />
+        <FrontPage />
+      </div>
+    </ContextProvider>
+  );
+}
+
+export default App;
